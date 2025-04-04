58670b530cb5e34fde5c994e1093730b2e4ffdd1
EzequielCuellar <ezequielalexiscuellar@gmail.com>
2025-04-04 12:22:05 -0300

    Ejercicio 1

---
+++ Dia 3-4-2025/Ejercicio 1.py
@@ -0,0 +1,14 @@
+
+nombre = input("Por favor, ingresa tu nombre: ")
+apellido = input("Por favor, ingresa tu apellido: ")
+edad = input("Por favor, ingresa tu edad: ")
+correo = input("Por favor, ingresa tu correo electrónico: ")
+
+
+print("\n" + "=" * 40)
+print("Tarjeta de presentacion")
+print("\n" + "=" * 40)
+print("nombre:", nombre)
+print("apellido:", apellido)
+print("Edad:", edad)
+print("correo:", correo)
--- Dia 3-4-2025/tempCodeRunnerFile.py
+++ Dia 3-4-2025/tempCodeRunnerFile.py
@@ -1,6 +1,17 @@
-print("ingrese su nombre")
-nombre=input()
-print("ingrese su edad")
-edad=input()
-print("ingrese su direccion")
-direccion=input()
+# Pedimos los datos al usuario
+nombre = input("Por favor, ingresa tu nombre: ")
+apellido = input("Por favor, ingresa tu apellido: ")
+edad = input("Por favor, ingresa tu edad: ")
+correo = input("Por favor, ingresa tu correo electrónico: ")
+
+# Mostramos los datos en forma de tarjeta de presentación
+print("\n" + "=" * 40)
+print("        TARJETA DE PRESENTACIÓN")
+print("=" * 40)
+print("Nombre:     ", nombre, apellido)
+print("Edad:       ", edad, "años")
+print("Correo:     ", correo)
+print("=" * 40)
+
+
+
